29a4060929d2a296081ca3021519f849349ffe5b
hukko00 <hukkomaccho@outlook.jp>
2026-03-18 20:53:41 +0900

    Initial Commit

---
+++ Asobee/AsobeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsobeeApp.swift
+//  Asobee
+//
+//  Created by 鈴木勝博 on 2026/03/18.
+//
+
+import SwiftUI
+
+@main
+struct AsobeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
