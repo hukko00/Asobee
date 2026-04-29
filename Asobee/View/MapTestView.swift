c201bcac5f8d2002441f2a98938aae6513622ec2
hukko00 <hukkomaccho@outlook.jp>
2026-04-29 12:43:15 +0900

    Test

---
+++ Asobee/View/MapTestView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct MapTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapTestView()
+}
